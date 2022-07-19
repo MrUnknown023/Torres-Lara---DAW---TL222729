3175572d8810151874cc312d2b6f248f03cfab15
MrUnknown023 <90297051+MrUnknown023@users.noreply.github.com>
2022-07-19 07:14:44 -0600

    Update complementario.js

--- Guia 2/Complementarios/js/complementario.js
+++ Guia 2/Complementarios/js/complementario.js
@@ -4,22 +4,19 @@ var cantCifras;
 var cantCifIm = 0, cantCifPar = 0;
 var sumaImp = 0, sumPar = 0, sumTot = 0;
 var cifraM, cifraM;
-var flag = false;
-var k=0;
 
 //Ingreso de datos
 num = prompt("Digite un numero entero:","");
 
 //Validacion para cantidad de cifras totales
 if(isNaN(num)){
-        alert("ERROR.El dato ingresado no es un número....");
-    }else if(!(num)){
-        alert("ERROR.Campo vacio...");
-    }else if(num > 0){
-        cantCifras = num.length;
-        document.write("<h3>La cantidad de cifras ingresadas es: " + cantCifras + "</h3>");
-    }
-    
+    alert("ERROR.El dato ingresado no es un número....");
+}else if(!(num)){
+    alert("ERROR.Campo vacio...");
+}else if(num > 0){
+    cantCifras = num.length;
+    document.write("<h3>La cantidad de cifras ingresadas es: " + cantCifras + "</h3>");
+}
 
 
 
